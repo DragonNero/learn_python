75b66c372a7198ed71ea83dcf43fff5a3de81a38
DragonNero <katdegaulle@gmail.com>
2021-01-14 00:48:23 +0100

    Replace strings stupid for smart

---
+++ ex_06/ex_06_03.py
@@ -0,0 +1,9 @@
+name = 'Dragon is stupid'
+letterNumber = name.find('gon')
+print(letterNumber)
+
+letterNumber2 = name.find('z')
+print(letterNumber2)
+
+replaceSmart = name.replace('stupid', 'smart')
+print(replaceSmart)
