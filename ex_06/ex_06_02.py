0c10ab52770512e711f9d2e4da92fd815f704a06
DragonNero <katdegaulle@gmail.com>
2021-01-14 00:20:49 +0100

    Dragon wants too eat. What you will give for Dragon? Strings

---
+++ ex_06/ex_06_02.py
@@ -0,0 +1,13 @@
+dragonWants = input('Dragon wants to eat: ')
+
+if dragonWants.lower() == 'salmon':
+    print('Dragon is happy and eat ' + dragonWants + ' You are good boy')
+
+elif dragonWants.lower() == 'chicken':
+    print('Dragon is sad because you gave him ' + dragonWants + ' But not really')
+
+elif dragonWants.lower() == 'poop':
+    print('You gave Dragon ' + dragonWants + '. Dragon pukes on you')
+
+else:
+    print('Give me a salmon, skin bag,I want salmon! Meow')
