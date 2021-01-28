013e092e8410f1e9ec9e61dfac8ab3c78b9b11ac
DragonNero <katdegaulle@gmail.com>
2021-01-28 20:13:03 +0100

    Regular expressions

---
+++ ex_11/ex_11_04.py
@@ -0,0 +1,10 @@
+import re
+hand = open('mbox-short.txt')
+numList = list()
+for line in hand:
+    line = line.rstrip()
+    stuff = re.findall('X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(stuff) !=1: continue
+    num = float(stuff[0])
+    numList.append(num)
+print('Maximum:', max(numList))
