db7963ea49e9ecd8d69730153a5e973a968f0904
DragonNero <katdegaulle@gmail.com>
2021-02-07 20:00:46 +0100

    started object oriented

---
+++ ex_14/ex_14_01.py
@@ -0,0 +1,11 @@
+stuff = list()
+stuff.append('python')
+stuff.append('chuck')
+stuff.sort()
+#print (stuff[0])
+#print (stuff.__getitem__(0))
+#print (list.__getitem__(stuff,0))
+#dir(stuff)
+#print(dir(stuff))
+x = 5
+print(x.__subclasshook__)
