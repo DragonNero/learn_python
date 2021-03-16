022dfed34f105b24d4e40a175b43aa7c3cbbe241
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2021-03-16 22:15:26 +0100

    Travel list beginning

---
+++ Travel-list/Check_list.py
@@ -0,0 +1,7 @@
+swimming = ['swimsuit', 'sunscreen', 'towel', 'snorkling_mask']
+dragon_travel = ['passport', 'litter', 'food', 'toy', 'clothes', 'shampoo', 'sponge', 'ear_cleaner', 'eyes_cleaner', 'digestive_pills']
+cold_country= ['sweater', 'warm_coat', 'winter_boots', 'winter_pants']
+hiking = ['hiking_boots', 'sunscreen']
+diving = ['equipment']
+
+print(swimming)
