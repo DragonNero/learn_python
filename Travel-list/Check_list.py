1ba49971d4789286de36effc42901cdbfe811ed3
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2021-03-18 17:52:09 +0100

    All lists are inside dictionary and if input one specific key it prints values of this key

--- Travel-list/Check_list.py
+++ Travel-list/Check_list.py
@@ -1,14 +1,32 @@
-swimming = ['swimsuit', 'sunscreen', 'towel', 'snorkling_mask']
-dragon_travel = ['passport', 'litter', 'food', 'toy', 'clothes', 'shampoo', 'sponge', 'ear_cleaner', 'eyes_cleaner', 'digestive_pills']
-cold_country= ['sweater', 'warm_coat', 'winter_boots', 'winter_pants']
-hiking = ['hiking_boots', 'sunscreen']
-diving = ['equipment']
-
 # = - an assignment(is)    == - comparison
+checkList = {
+    'swimming' : [
+        'swimsuit',
+        'sunscreen',
+        'towel',
+        'snorkling_mask'
+    ],
+    'dragon_travel' : [
+        'passport',
+        'litter',
+        'food',
+        'toy',
+        'clothes', 'shampoo', 'sponge', 'ear_cleaner', 'eyes_cleaner',
+        'digestive_pills'
+    ],
+    'cold_country' : [
+        'sweater', 'warm_coat', 'winter_boots', 'winter_pants'
+    ],
+    'hiking' : [
+        'hiking_boots', 'sunscreen'
+    ],
+    'diving' : [
+        'equipment'
+    ]
+}
 inp = input('Where are you going:')
-if inp == 'swimming':
-    print(swimming)
-elif inp == 'dragon_travel':
-    print(dragon_travel)
+inp = inp.strip()
+if inp in checkList:
+    print(checkList.get(inp))
 else:
     print("Not found")
