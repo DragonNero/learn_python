19c38fdb3f2e95fd02afc244e41925db84881c6a
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2021-03-25 18:09:06 +0100

    added possibility to select items or categories in opened window

--- Travel-list/Manage_Items.py
+++ Travel-list/Manage_Items.py
@@ -1,17 +1,27 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 app = QApplication(sys.argv)
 
+def selectionchange(text):
+    labelBox.setText(text)
+    print(text)
+
 window = QWidget()
 window.setWindowTitle('Manage items and categories')
-window.setGeometry(0, 0, 400, 80)
-helloMsg = QLabel('<h1>Hello I am Dragon!</h1>', parent=window)
-helloMsg.move(60, 15)
+#window.setGeometry(0, 0, 400, 80)
+layout = QGridLayout()
+selectBox = QComboBox()
+selectBox.addItems(["Categories", "Items"])
+selectBox.currentTextChanged.connect(selectionchange)
+layout.addWidget(selectBox, 0, 0)
+labelBox = QLabel('<h1>Hello I am Dragon!</h1>')
+layout.addWidget(labelBox, 1, 0)
 
+window.setLayout(layout)
 window.show()
 
 sys.exit(app.exec_())
