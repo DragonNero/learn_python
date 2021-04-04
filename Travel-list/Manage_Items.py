ceab8192e6161eb8662563349aee63a280ba6c79
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2021-04-04 17:37:51 +0200

    Classes from https://www.reddit.com/r/pyqt/comments/acjrjc/pyqt5_loading_multiple_windows/

--- Travel-list/Manage_Items.py
+++ Travel-list/Manage_Items.py
@@ -1,12 +1,8 @@
-import sys
-import sqlite3
+import sys, sqlite3
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-database = sqlite3.connect('Travel_list.db')
-cur = database.cursor()
+"""
 
 app = QApplication(sys.argv)
 
@@ -81,23 +77,150 @@ def addWindow():
 def closeWindow():
     addWindow.close()
 
-window = QWidget()
-window.setWindowTitle('Manage items and categories')
-layout = QGridLayout()
-selectBox = QComboBox()
-selectBox.addItems(["Categories", "Items"])
-selectBox.currentTextChanged.connect(selectionChange)
-layout.addWidget(selectBox, 0, 0)
+"""
 
-addButton = QPushButton('Add')
-addButton.clicked.connect(addWindow)
-layout.addWidget(addButton, 0, 1)
+database = sqlite3.connect('Travel_list.db')
+cur = database.cursor()
 
-labelBox = QLabel('<h1>Categories</h1>')
-layout.addWidget(labelBox, 1, 0)
-layout.addWidget(displayCategoryTable(), 2, 0, 1, 2)
 
-window.setLayout(layout)
-window.show()
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
 
-sys.exit(app.exec_())
+        MainWindow.setObjectName('Manage items and categories')
+        MainWindow.resize(400, 300)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+
+        self.layout = QtWidgets.QGridLayout()
+        self.selectBox = QtWidgets.QComboBox()
+        self.selectBox.addItems(["Categories", "Items"])
+        self.selectBox.currentTextChanged.connect(self.selectionChange)
+        self.layout.addWidget(self.selectBox, 0, 0)
+
+        self.addButton = QtWidgets.QPushButton(self.centralWidget)
+        self.addButton.clicked.connect(addWindow)
+        self.addButton.setObjectName("Add")
+        self.layout.addWidget(self.addButton, 0, 1)
+
+        self.labelBox = QtWidgets.QLabel('<h1>Categories</h1>')
+        self.layout.addWidget(self.labelBox, 1, 0)
+        self.layout.addWidget(displayCategoryTable(), 2, 0, 1, 2)
+
+        MainWindow.setCentralWidget(self.layout)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.addButton.setText(_translate("MainWindow", "LoadDialogWindow"))
+
+    def LoadDialogWindow(self):
+        DialogWindow = QtWidgets.QMainWindow()
+        ui = Ui_DialogWindow()
+        ui.setupUi(DialogWindow)
+        DialogWindow.show()
+
+    def displayCategoryTable(self):
+        cur.execute('SELECT id, title FROM category')
+        categories = cur.fetchall()
+
+        categoriesTable = QTableWidget()
+        categoriesTable.setRowCount(len(categories)+1)
+        categoriesTable.setColumnCount(2)
+        categoriesTable.setItem(0,0,QTableWidgetItem('Id'))
+        categoriesTable.setItem(0,1,QTableWidgetItem('Title'))
+
+        numberOfRow = 1
+        for category in categories:
+            categoriesTable.setItem(numberOfRow,0,QTableWidgetItem(str(category[0])))
+            categoriesTable.setItem(numberOfRow,1,QTableWidgetItem(category[1]))
+            numberOfRow  = numberOfRow  + 1
+
+        return categoriesTable
+
+    def displayItemTable(self):
+        cur.execute('SELECT i.id, c.title AS category, i.title AS item FROM item i LEFT JOIN category c ON c.id = i.category_id')
+        items = cur.fetchall()
+
+        itemsTable = QTableWidget()
+        itemsTable.setRowCount(len(items)+1)
+        itemsTable.setColumnCount(3)
+        itemsTable.setItem(0,0,QTableWidgetItem('Id'))
+        itemsTable.setItem(0,1,QTableWidgetItem('Category'))
+        itemsTable.setItem(0,2,QTableWidgetItem('Title'))
+
+        numberOfRow = 1
+        for item in items:
+            itemsTable.setItem(numberOfRow,0,QTableWidgetItem(str(item[0])))
+            itemsTable.setItem(numberOfRow,1,QTableWidgetItem(item[1]))
+            itemsTable.setItem(numberOfRow,2,QTableWidgetItem(item[2]))
+            numberOfRow  = numberOfRow  + 1
+
+        return itemsTable
+
+    def selectionChange(self, text):
+        layout.itemAt(3).widget().deleteLater()
+        if text == "Categories":
+            labelBox.setText('<h1>Categories</h1>')
+            layout.addWidget(self.displayCategoryTable(), 2, 0, 1, 2)
+        elif text == "Items":
+            labelBox.setText('<h1>Items</h1>')
+            layout.addWidget(self.displayItemTable(), 2, 0, 1, 2)
+        else:
+            print('Group is not defined')
+
+class Ui_DialogWindow(object):
+
+    def setupUi(self, DialogWindow):
+        DialogWindow.setObjectName("DialogWindow")
+        DialogWindow.resize(400, 300)
+        self.centralWidget = QtWidgets.QWidget(DialogWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 130, 191, 23))
+        self.pushButton.setObjectName("pushButton")
+        DialogWindow.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi(DialogWindow)
+        QtCore.QMetaObject.connectSlotsByName(DialogWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("DialogWindow", "DialogWindow"))
+        self.pushButton.setText(_translate("DialogWindow", "Congratz !"))
+
+
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def Show_MainWindow(self):
+
+        self.MainWindow = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.MainWindow)
+        self.ui.addButton.clicked.connect(self.Show_DialogWindow)
+
+        self.MainWindow.show()
+
+    def Show_DialogWindow(self):
+
+        self.DialogWindow = QtWidgets.QMainWindow()
+        self.ui = Ui_DialogWindow()
+        self.ui.setupUi(self.DialogWindow)
+        self.ui.pushButton.clicked.connect(self.Print)
+
+        self.DialogWindow.show()
+
+    def Print(self):
+        print('After 99 hours of trying out everything')
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    Controller = Controller()
+    Controller.Show_MainWindow()
+    sys.exit(app.exec_())
