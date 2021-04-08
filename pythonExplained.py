5266134375170caa222f0272a71e98ccbe49dc12
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2021-04-08 17:32:44 +0200

    python basics again

---
+++ pythonExplained.py
@@ -0,0 +1,24 @@
+
+#function - we use to do manipulation. Commands abbriviation
+def getName():
+    return 'Katia'
+
+print 'Hello' + getName() + '!'
+
+
+
+
+
+#variable  - box with stuff inside!
+name = 'Katia'
+
+print 'Hello' + name + '!'
+
+
+
+#combination
+def greeting(firstname):
+    return 'hello' + firstname + '!'
+
+name = 'Katia'
+print greeting(name)
