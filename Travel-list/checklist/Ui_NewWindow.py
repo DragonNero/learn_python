6c79a165bfdcc46f3193f32bc1b2a86550f13f11
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2021-05-10 17:53:32 +0200

    Interface of checklist has now checkboxes of categories

--- Travel-list/checklist/Ui_NewWindow.py
+++ Travel-list/checklist/Ui_NewWindow.py
@@ -14,14 +14,24 @@ class Ui_NewWindow(object):
 
         self.layout = QtWidgets.QGridLayout()
         #TODO fill the interface
-        self.labelBox = QtWidgets.QLabel('<h1>Travel check list</h1>')
-        self.layout.addWidget(self.labelBox, 0, 0, 1, 2)
+        self.labelBox = QtWidgets.QLabel('<h1>Create a new check list</h1>')
+        self.layout.addWidget(self.labelBox, 0, 0, 1, 4)
 
-        self.newButton = QtWidgets.QPushButton(self.centralWidget)
-        self.layout.addWidget(self.newButton, 1, 0)
+        self.sublabelBox = QtWidgets.QLabel('Select what you will do:')
+        self.layout.addWidget(self.sublabelBox, 1, 0, 1, 4)
 
-        self.openButton = QtWidgets.QPushButton(self.centralWidget)
-        self.layout.addWidget(self.openButton, 1, 1)
+        rowCounter = 2
+        self.cur.execute('SELECT id, title FROM category ORDER BY title ASC')
+        categories = self.cur.fetchall()
+        for index, category in enumerate(categories):
+            colCounter = index % 4
+            self.categoryCheck = QtWidgets.QCheckBox(category[1])
+            self.layout.addWidget(self.categoryCheck, rowCounter, colCounter)
+            if colCounter == 3:
+                rowCounter = rowCounter + 1
+
+        self.startButton = QtWidgets.QPushButton(self.centralWidget)
+        self.layout.addWidget(self.startButton, rowCounter+1, 4)
 
         self.centralWidget.setLayout(self.layout)
         NewWindow.setCentralWidget(self.centralWidget)
@@ -33,5 +43,4 @@ class Ui_NewWindow(object):
     def retranslateUi(self, NewWindow):
         _translate = QtCore.QCoreApplication.translate
         NewWindow.setWindowTitle("Create new checklist")
-        self.newButton.setText("Create new checklist")
-        self.openButton.setText("Open an existing checklist")
+        self.startButton.setText("Start")
